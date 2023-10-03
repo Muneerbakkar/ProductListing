69bc6294018aa5b2b062d5f9096de376cef4cc1c
Muneer Bakkar <112262924+Muneerbakkar@users.noreply.github.com>
2023-10-03 13:31:34 +0530

    Third commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,47 @@
-import ProductList from "@/components/ProductList";
+import Product from "@/components/Products";
 
-export default function Home() {
+
+const getProducts = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/products", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch products");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading products", error);
+  }
+};
+
+const getCategories = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/categories", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch categories");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading categories", error);
+  }
+};
+
+
+export default async function ProductList() {
+  const { products } = await getProducts();
+  const { categories } = await getCategories();
+ 
+
   return (
     <>
-      <ProductList />
+      <Product products={products} categories={categories} />
     </>
   );
 }
